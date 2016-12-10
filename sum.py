c28d5978f43f1d5b529d9af1f4592f203a2f19ee
jean-gert nesselbosch <nesselbosch@strato-rz.de>
2016-12-10 21:14:02 +0100

    zeugs

---
+++ sum.py
@@ -0,0 +1,13 @@
+#sum.py
+
+def add_two_numbers(a, b):
+    '''
+        Function to add two numbers
+    '''
+    addition = a + b
+    return addition
+
+if __name__ == '__main__':
+    a = 5
+    b = 7
+    print add_two_numbers(a, b)
